180580b97a5068ae0402cccfbaa0b00caf36fa64
Johannes <65119813+Johannes29@users.noreply.github.com>
2023-12-08 15:55:23 +0100

    Add test for when task is created incorrectly

--- tests/test_api.py
+++ tests/test_api.py
@@ -21,7 +21,11 @@ class TestAPI(unittest.TestCase):
         
         self.assertEqual(201, response.status_code)
         self.assertIn(task_name, response.text)
-        
+    
+    def test_create_task_with_invalid_request(self):
+        response = requests.post(BASE_URL + "/api/new_task", data="")
+        self.assertEqual(400, response.status_code)
+
     @classmethod
     def tearDownClass(cls):
         cls.server_process.terminate()
