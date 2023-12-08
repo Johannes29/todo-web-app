8e380801c9f5d58da62f3baa775f5b4611917228
Johannes <65119813+Johannes29@users.noreply.github.com>
2023-12-08 15:46:58 +0100

    Add test for creating a task

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,4 @@
+import json
 import unittest
 import subprocess
 import requests
@@ -8,6 +9,18 @@ class TestAPI(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.server_process = subprocess.Popen("flask --app api/main run --host localhost --port 5678")
+
+    # TODO Implement create task feature so that this test passes
+    def test_create_task(self):
+        task_name = "Test task 123"
+        request_body = json.dumps({
+            "name": task_name
+        })
+
+        response = requests.post(BASE_URL + "/api/new_task", data=request_body)
+        
+        self.assertEqual(201, response.status_code)
+        self.assertIn(task_name, response.text)
 
     # This test is just a placeholder for future tests
     def test_hello_world(self):
