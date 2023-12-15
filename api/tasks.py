73e0d9d08b0e3c9b1d4df9162f5d4cca7c3f7bc7
Johannes <65119813+Johannes29@users.noreply.github.com>
2023-12-15 14:24:57 +0100

    Add class for keeping track of tasks

---
+++ api/tasks.py
@@ -0,0 +1,14 @@
+class Tasks:
+    def __init__(self):
+        self.tasks = []
+        self.next_id = 0
+
+    def add_task(self, name: str):
+        new_task = {"name": name, "id": self.new_task_id()}
+        self.next_id += 1
+        self.tasks.append(new_task)
+
+    def new_task_id(self):
+        return_id = self.next_id
+        self.next_id += 1
+        return return_id
