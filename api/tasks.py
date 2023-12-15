fb8a8538d4bcfd0c2d6e6cf89ebd63262bf0e2ff
Johannes <65119813+Johannes29@users.noreply.github.com>
2023-12-15 14:51:44 +0100

    Modify add_task method to return task id

--- api/tasks.py
+++ api/tasks.py
@@ -7,6 +7,7 @@ class Tasks:
         new_task = {"name": name, "id": self._new_task_id()}
         self.next_id += 1
         self.tasks.append(new_task)
+        return new_task["id"]
 
     def _new_task_id(self):
         return_id = self.next_id
