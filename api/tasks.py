c1b8a6119eee1e8c13763ddfe2b541c95726e7da
Johannes <65119813+Johannes29@users.noreply.github.com>
2023-12-15 14:45:42 +0100

    Make method in Tasks class "private"

--- api/tasks.py
+++ api/tasks.py
@@ -4,11 +4,11 @@ class Tasks:
         self.next_id = 0
 
     def add_task(self, name: str):
-        new_task = {"name": name, "id": self.new_task_id()}
+        new_task = {"name": name, "id": self._new_task_id()}
         self.next_id += 1
         self.tasks.append(new_task)
 
-    def new_task_id(self):
+    def _new_task_id(self):
         return_id = self.next_id
         self.next_id += 1
         return return_id
