e0497fb0e8ef74f0adde9fa15e7bd77c7933ebfc
Johannes <65119813+Johannes29@users.noreply.github.com>
2023-12-15 15:56:29 +0100

    Implement functionality to add task

--- api/main.py
+++ api/main.py
@@ -1,7 +1,14 @@
-from flask import Flask
+import json
+
+from flask import Flask, make_response, request
+from tasks import Tasks
 
 app = Flask(__name__)
+tasks = Tasks()
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/api/new_task", methods=["GET", "POST"])
+def create_new_task():
+    task_name = request.get_json()["name"]
+    task_id = tasks.add_task(task_name)
+    task = tasks.get_task(task_id)
+    return make_response(json.dumps(task), 201)
